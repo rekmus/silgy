8aa22ef1c99ee08c5e02a1feba13899afa1692f0
Jurek <36708070+silgy@users.noreply.github.com>
2018-03-16 13:34:33 +0100

    Add files via upload

--- src/silgy_watcher.c
+++ src/silgy_watcher.c
@@ -5,9 +5,10 @@
 
 #include "silgy.h"
 
+#define BUFSIZE       8196
 
-#define STOP_COMMAND  "sudo silgystop"
-#define START_COMMAND "sudo silgystart"
+#define STOP_COMMAND  "sudo $SILGYDIR/bin/silgystop"
+#define START_COMMAND "sudo $SILGYDIR/bin/silgystart"
 
 
 char G_logLevel=4;
@@ -18,6 +19,7 @@ time_t G_now;
 char G_appdir[256];
 char G_dt[20];
 char G_tmp[1048576];
+char *G_shm_segptr;
 
 
 void restart(void);
@@ -28,6 +30,12 @@ void restart(void);
 -------------------------------------------------------------------------- */
 int main(int argc, char *argv[])
 {
+    int sockfd;
+    int conn;
+    int bytes;
+    char buffer[BUFSIZE];
+    static struct sockaddr_in serv_addr;
+
     lib_get_app_dir();      // set G_appdir
 
     /* init time variables */
@@ -41,18 +49,75 @@ int main(int argc, char *argv[])
     if ( !log_start("watch", TRUE) )
         return -1;
 
-
+    /* -------------------------------------------------------------------------- */
 
     ALWAYS("Trying to connect...");
 
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+    {
+        ERR("socket failed, errno = %d (%s)", errno, strerror(errno));
+        exit(1);
+    }
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_port = htons(80);
+
+	if ( (conn=connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0 )
+    {
+        ERR("connect failed, errno = %d (%s)", errno, strerror(errno));
+        close(sockfd);
+        ALWAYS("Restarting...");
+        restart();
+        log_finish();
+        return 0;
+    }
 
     ALWAYS("Connected");
 
+    /* -------------------------------------------------------------------------- */
+
     ALWAYS("Sending request...");
+
+    char *p=buffer;     /* stpcpy is more convenient and faster than strcat */
+
+    p = stpcpy(p, "GET / HTTP/1.1\r\n");
+    p = stpcpy(p, "Host: 127.0.0.1\r\n");
+    p = stpcpy(p, "User-Agent: Silgy Watcher (Bot)\r\n");   /* don't bother Silgy with creating a user session */
+    p = stpcpy(p, "Connection: close\r\n");
+    p = stpcpy(p, "\r\n");
+
+	bytes = write(sockfd, buffer, strlen(buffer));
+
+    if ( bytes < 18 )
+    {
+        ERR("write failed, errno = %d (%s)", errno, strerror(errno));
+        close(conn);
+        close(sockfd);
+        log_finish();
+        return 0;
+    }
+
+    /* -------------------------------------------------------------------------- */
 
     ALWAYS("Reading response...");
 
-    ALWAYS("Response OK");
+	bytes = read(sockfd, buffer, BUFSIZE);
+
+    if ( bytes > 7 && 0==strncmp(buffer, "HTTP/1.1", 8) )
+    {
+        ALWAYS("Response OK");
+    }
+    else
+    {
+        ALWAYS("Response NOT OK, restarting...");
+        restart();
+    }
+
+    /* -------------------------------------------------------------------------- */
+
+	close(conn);
+	close(sockfd);
 
     log_finish();
 
