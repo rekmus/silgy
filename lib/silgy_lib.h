3d4e08a1a0cd0bffd687f8b1014a8a4a655ed055
Jurek Muszyński <36708070+silgy@users.noreply.github.com>
2019-03-20 13:30:14 +0100

    MinGW clock_gettime() conflict

--- lib/silgy_lib.h
+++ lib/silgy_lib.h
@@ -346,7 +346,10 @@ extern "C" {
 
 #ifdef _WIN32   /* Windows */
     int getpid(void);
-    int clock_gettime(int dummy, struct timespec *spec);
+#ifdef _MSC_VER /* Microsoft compiler */
+typedef int clockid_t;
+#endif /* _MSC_VER */
+    int clock_gettime_win(clockid_t clockid, struct timespec *spec);
     char *stpcpy(char *dest, const char *src);
     char *stpncpy(char *dest, const char *src, unsigned int len);
 #endif  /* _WIN32 */
