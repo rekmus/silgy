ba3eef8a0bfd2dbaea3659a433df803ae26de277
Jurek Muszyński <36708070+silgy@users.noreply.github.com>
2019-03-20 13:50:56 +0100

    vcvars32 tip

--- src/m_vc.bat
+++ src/m_vc.bat
@@ -8,4 +8,8 @@ cl silgy_app.cpp ^
 -I . -I ..\lib ^
 /Fe..\bin\silgy_app
 
+echo.
+
 echo Make sure you have dirent.h from https://github.com/tronkko/dirent/tree/master/include
+
+echo Remember to set the environment with vcvars32 (path: C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin)
