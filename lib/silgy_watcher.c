c7e48fd63161a61430cdd1c58172eea0ddae34b3
Jurek <jmuszynski@man.poznan.pl>
2019-08-29 12:43:53 +0200

    silgy_watcher using CALL_HTTP

--- lib/silgy_watcher.c
+++ lib/silgy_watcher.c
@@ -7,12 +7,6 @@
 #include "silgy.h"
 
 
-#define BUFSIZE             8196
-
-#define REASON_CONNECT      1
-#define REASON_WRITE        2
-#define REASON_READ         3
-
 #define STOP_COMMAND        "sudo $SILGYDIR/bin/silgystop"
 #define START_COMMAND       "sudo $SILGYDIR/bin/silgystart"
 
@@ -32,7 +26,43 @@ static int  M_watcherWait;
 static int  M_watcherLogRestart;
 
 
-void restart(char reason);
+/* --------------------------------------------------------------------------
+   Restart
+-------------------------------------------------------------------------- */
+void restart()
+{
+    if ( M_watcherLogRestart > 0 )
+    {
+        G_logLevel = M_watcherLogRestart;
+        log_start("watcher", FALSE);
+    }
+
+    ALWAYS_T("Restarting...");
+
+    INF_T("Stopping...");
+    INF_T(M_watcherStopCmd);
+    system(M_watcherStopCmd);
+
+    lib_update_time_globals();
+
+    INF_T("Waiting %d second(s)...", M_watcherWait);
+    sleep(M_watcherWait);
+
+    lib_update_time_globals();
+
+    INF_T("Starting...");
+    INF_T(M_watcherStartCmd);
+    system(M_watcherStartCmd);
+
+#ifdef APP_ADMIN_EMAIL
+    if ( strlen(APP_ADMIN_EMAIL) )
+    {
+        char message[1024];
+        strcpy(message, "Silgy Watcher had to restart web server.");
+        silgy_email(APP_ADMIN_EMAIL, "Silgy restart", message);
+    }
+#endif
+}
 
 
 /* --------------------------------------------------------------------------
@@ -41,11 +71,6 @@ void restart(char reason);
 int main(int argc, char *argv[])
 {
     char config[256];
-    int  sockfd;
-    int  conn;
-    int  bytes;
-    char buffer[BUFSIZE];
-static struct sockaddr_in serv_addr;
 
     /* library init ------------------------------------------------------ */
 
@@ -56,9 +81,6 @@ static struct sockaddr_in serv_addr;
     G_initialized = 1;
 
     /* read the config file or set defaults ------------------------------ */
-
-    char exec_name[256];
-    lib_get_exec_name(exec_name, argv[0]);
 
     if ( G_appdir[0] )
     {
@@ -105,128 +127,30 @@ static struct sockaddr_in serv_addr;
 
     /* ------------------------------------------------------------------- */
 
-    INF("Trying to connect...");
+    INF_T("Trying to connect...");
 
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+    G_RESTTimeout = 60000;   /* 60 seconds */
+
+    char url[1024];
+
+    sprintf(url, "127.0.0.1:%d", G_httpPort);
+
+    REST_HEADER_SET("User-Agent", "Silgy Watcher Bot");
+
+    if ( !CALL_REST_HTTP(NULL, NULL, "GET", url, 0) )
     {
-        ERR("socket failed, errno = %d (%s)", errno, strerror(errno));
-        log_finish();
-		return EXIT_FAILURE;
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(G_httpPort);
-
-    if ( (conn=connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0 )
-    {
-        ERR("connect failed, errno = %d (%s)", errno, strerror(errno));
-        close(sockfd);
-        restart(REASON_CONNECT);
-        log_finish();
-        return EXIT_SUCCESS;
-    }
-
-    INF("Connected");
-
-    /* ------------------------------------------------------------------- */
-
-    INF("Sending request...");
-
-    char *p=buffer;     /* stpcpy is more convenient and faster than strcat */
-
-    p = stpcpy(p, "GET / HTTP/1.1\r\n");
-    p = stpcpy(p, "Host: 127.0.0.1\r\n");
-    p = stpcpy(p, "User-Agent: Silgy Watcher Bot\r\n");   /* don't bother Silgy with creating a user session */
-    p = stpcpy(p, "Connection: close\r\n");
-    p = stpcpy(p, "\r\n");
-
-    bytes = write(sockfd, buffer, strlen(buffer));
-
-    if ( bytes < 18 )
-    {
-        ERR("write failed, errno = %d (%s)", errno, strerror(errno));
-        close(conn);
-        close(sockfd);
-        restart(REASON_WRITE);
-        log_finish();
-        return EXIT_SUCCESS;
+        lib_update_time_globals();
+        ERR_T("Couldn't connect");
+        restart();
     }
 
     /* ------------------------------------------------------------------- */
 
-    INF("Reading response...");
+    lib_update_time_globals();
 
-    bytes = read(sockfd, buffer, BUFSIZE);
-
-    if ( bytes > 7 && 0==strncmp(buffer, "HTTP/1.1", 8) )
-    {
-        INF("Response OK");
-    }
-    else
-    {
-        ERR("read failed, errno = %d (%s)", errno, strerror(errno));
-        close(conn);
-        close(sockfd);
-        restart(REASON_READ);
-        log_finish();
-        return EXIT_SUCCESS;
-    }
-
-    /* ------------------------------------------------------------------- */
-
-    close(conn);
-    close(sockfd);
+    INF_T("silgy_watcher ended");
 
     log_finish();
 
     return EXIT_SUCCESS;
-}
-
-
-/* --------------------------------------------------------------------------
-   Restart
--------------------------------------------------------------------------- */
-void restart(char reason)
-{
-    if ( M_watcherLogRestart > 0 )
-    {
-        G_logLevel = M_watcherLogRestart;
-        log_start("watcher", FALSE);
-    }
-
-    char reason_desc[256];
-
-    if ( reason == REASON_CONNECT )
-        strcpy(reason_desc, "Couldn't connect");
-    else if ( reason == REASON_WRITE )
-        strcpy(reason_desc, "Couldn't send the request");
-    else if ( reason == REASON_READ )
-        strcpy(reason_desc, "Couldn't read the response");
-    else
-        strcpy(reason_desc, "Unknown reason!");
-
-    ALWAYS(reason_desc);
-
-    ALWAYS("Restarting...");
-
-    INF("Stopping...");
-    INF(M_watcherStopCmd);
-    system(M_watcherStopCmd);
-
-    INF("Waiting %d second(s)...", M_watcherWait);
-    sleep(M_watcherWait);
-
-    INF("Starting...");
-    INF(M_watcherStartCmd);
-    system(M_watcherStartCmd);
-
-#ifdef APP_ADMIN_EMAIL
-    if ( strlen(APP_ADMIN_EMAIL) )
-    {
-        char message[1024];
-        sprintf(message, "Silgy Watcher had to restart web server due to: %s", reason_desc);
-        silgy_email(APP_ADMIN_EMAIL, "Silgy restart", message);
-    }
-#endif
 }
